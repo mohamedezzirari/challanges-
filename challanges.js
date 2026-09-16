c0dd0d1da68b51f943242635072cf646c66a9ba2
mohamedezzirari <mohamedezzirari@pronton.me>
2026-09-16 17:58:26 +0200

    challange 1

---
+++ challanges.js
@@ -0,0 +1,10 @@
+let Pseudo = "tirari"
+let Level = 28
+let Age = 26
+let Score = 1000
+let Pays = "Maroc"
+console.log("Pseudo : " + Pseudo)
+console.log("Niveau : " + Level)
+console.log("Âge : " + Age)
+console.log("Score : " + Score)
+console.log("Pays : " + Pays)
